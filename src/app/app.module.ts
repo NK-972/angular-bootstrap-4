4d7c6b9909c140f7c8095fae4ba6ce79bd04d656
NK-972 <nicolascastry@gmail.com>
2019-12-31 12:07:58 +0100

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,13 @@ import { ProduitsComponent } from './produits/produits.component';
 import { ProduitComponent } from './produit/produit.component';
 import { CenterService } from './Services/center.service';
 import { InstallationsComponent } from './installations/installations.component';
-import { UsineComponent } from './usine/usine.component';
 import { InstallationUsineComponent } from './installation-usine/installation-usine.component';
 import { InstallationEntrepotComponent } from './installation-entrepot/installation-entrepot.component';
 import { ServeurComponent } from './serveur/serveur.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent, ProduitsComponent, ProduitComponent, InstallationsComponent, UsineComponent, InstallationUsineComponent, InstallationEntrepotComponent, ServeurComponent],
+  declarations: [AppComponent, HeaderComponent, ProduitsComponent, ProduitComponent, InstallationsComponent, InstallationUsineComponent, InstallationEntrepotComponent, ServeurComponent],
   bootstrap: [AppComponent],
   providers: [CenterService]
 })
