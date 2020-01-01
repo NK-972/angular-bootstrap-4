2e8d4d925a6f47c2ed2196399eb6705f872dd1fa
NK-972 <nicolascastry@gmail.com>
2020-01-01 17:54:48 +0100

    2 files modified

--- src/app/Services/center.service.ts
+++ src/app/Services/center.service.ts
@@ -22,7 +22,7 @@ export class CenterService {
   panel_factory: JSON;
   key_panel_factory: string[];
   /* Installations - Entrepôt */
-  string_entrepot: string = "[[JSON, Tour, Nom de l'installation, N° Installation joueur, Taille, Région, Entretien, Indice salaire, Nombre d'employés, Nombre d'employés max, Production, Production possible, Indice technique, Consommation énergétique, Moral, Situation, Durée ammortissement, Temps avant utilisation, Valeur, Production max], [Usine n°1_0, 0, Usine n°1(U), 1, C, xxx, 0, 1.00, 0, 0, 0, 0, 0.00, 0, 0, fonctionnelle, 0, 0, 0, 0], [Usine n°1_1, 1, Usine n°1(U), 1, C, xxx, 40000, 1.00, 15, 0, 0, 0, 0.00, 0, 8, fonctionnelle, 0, 0, 0, 0], [Usine n°1_2, 2, Usine n°1(U), 1, C, xxx, 40000, 1.00, 15, 0, 0, 0, 0.00, 0, 8, fonctionnelle, 0, 0, 0, 0], [Usine n°1_3, 3, Usine n°1(U), 1, C, xxx, 50000, 3.25, 15, 0, 0, 0, 0.00, 0, 8, fonctionnelle, 0, 0, 0, 0], [Usine n°1_4, 4, Usine n°1(U), 1, C, xxx, 50000, 3.25, 15, 0, 0, 0, 0.00, 0, 8, fonctionnelle, 0, 0, 0, 0]]";
+  string_entrepot: string = "[[JSON, Tour, nom de l'entrepôt, N° entrepôt joueur, Taille, Région, Entretien, Indice salaire, Nombre d'employés, Nombre d'employés max, Stock, Taille du stockage, Indice technique, Frais divers, Situation, Moral, Valeur, Durée amortissement], [Entrepôt n°1_0, 0, Entrepôt n°1(e), 1, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0], [Entrepôt n°2_0, 0, Entrepôt n°2(e), 2, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0], [Entrepôt n°1_1, 1, Entrepôt n°1(e), 1, C, xxx, 0, 1, 5, 0, 0, 0, 0.00, 0, fonctionnelle, 2, 0, 0], [Entrepôt n°2_1, 1, Entrepôt n°2(e), 2, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0], [Entrepôt n°1_2, 2, Entrepôt n°1(e), 1, C, xxx, 0, 1, 5, 0, 0, 0, 0.00, 0, fonctionnelle, 2, 0, 0], [Entrepôt n°2_2, 2, Entrepôt n°2(e), 2, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0], [Entrepôt n°1_3, 3, Entrepôt n°1(e), 1, C, xxx, 30000, 1, 5, 0, 0, 0, 0.00, 0, fonctionnelle, 2, 0, 0], [Entrepôt n°2_3, 3, Entrepôt n°2(e), 2, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0], [Entrepôt n°1_4, 4, Entrepôt n°1(e), 1, C, xxx, 30000, 1, 5, 0, 0, 0, 0.00, 0, fonctionnelle, 2, 0, 0], [Entrepôt n°2_4, 4, Entrepôt n°2(e), 2, C, xxx, 0, 1, 0, 0, 0, 0, 0.00, 0, fonctionnelle, 0, 0, 0]]";
   json_entrepot: JSON;
   key_storage: string[];
   entrepot: Factory;
@@ -31,18 +31,8 @@ export class CenterService {
 
 
   constructor() {
-    
-    var JSONQury = {};
-    this.json_produits = this.utilitaire.StringToTable(this.string_produits);
-    this.key_product = Object.keys(this.json_produits);
-    let product_turn: string[] = this.utilitaire.getAllOcc(this.key_product, '_'+this.turn); 
-    for (var i = 0; i < product_turn.length; i++) {
-      JSONQury[product_turn[i].split('_')[0]] = this.createProduct(product_turn[i].split('_')[0], this.json_produits);
-    }
-    this.panel_produits = JSONQury as JSON;
-    this.key_panel_produits = Object.keys(this.panel_produits);
-    console.log(this.panel_produits);
-    
+    var JSONQury;
+    /*  */
     JSONQury = {};
     this.json_usines = this.utilitaire.StringToTable(this.string_usines);
     this.key_factory = Object.keys(this.json_usines);
@@ -53,7 +43,28 @@ export class CenterService {
     this.panel_factory = JSONQury as JSON;
     this.key_panel_factory = Object.keys(this.panel_factory);
     console.log(this.panel_factory);
-    /**/
+    /*  */
+    JSONQury = {};
+    this.json_entrepot = this.utilitaire.StringToTable(this.string_entrepot);
+    this.key_storage = Object.keys(this.json_entrepot);
+    let entrepot_turn: string[] = this.utilitaire.getAllOcc(this.key_storage, '_'+this.turn); 
+    for (var i = 0; i < entrepot_turn.length; i++) {
+      JSONQury[entrepot_turn[i].split('_')[0]] = this.createStorage(entrepot_turn[i].split('_')[0], this.json_entrepot);
+    }
+    this.panel_storage = JSONQury as JSON;
+    this.key_panel_storage = Object.keys(this.panel_storage);
+    console.log(this.panel_storage);
+    /*  */
+    JSONQury = {};
+    this.json_produits = this.utilitaire.StringToTable(this.string_produits);
+    this.key_product = Object.keys(this.json_produits);
+    let product_turn: string[] = this.utilitaire.getAllOcc(this.key_product, '_'+this.turn); 
+    for (var i = 0; i < product_turn.length; i++) {
+      JSONQury[product_turn[i].split('_')[0]] = this.createProduct(product_turn[i].split('_')[0], this.json_produits);
+    }
+    this.panel_produits = JSONQury as JSON;
+    this.key_panel_produits = Object.keys(this.panel_produits);
+    console.log(this.panel_produits);
   }
 
   createProduct(name: string, json:JSON): Product{
@@ -107,8 +118,8 @@ export class CenterService {
       , region: this.utilitaire.getData(json, name, "_"+this.turn, "Région")
       , IT: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Indice technique"))
       , FD: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Frais divers"))
-      , Stock: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Production"))
-      , SL: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Production max"))
+      , Stock: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Stock"))
+      , SL: Number(this.utilitaire.getData(json, name, "_"+this.turn, "Taille du stockage"))
       , S: Number(0)
     }
     return storage;
