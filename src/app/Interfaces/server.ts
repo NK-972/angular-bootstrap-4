d1516902d61c2660ce101ee51e22d7700bb94c92
NK-972 <nicolascastry@gmail.com>
2020-01-03 13:47:07 +0100

    5 files modified

--- src/app/Interfaces/server.ts
+++ src/app/Interfaces/server.ts
@@ -1,2 +1,9 @@
 export interface Server {
+  id: string
+  , debut: string
+  , nbrTourMax: Number
+  , Dtours: Number
+  , Fin: String
+  , nbrJoueur: Number
+  , Etat: string
 }
--- src/app/Services/init.service.ts
+++ src/app/Services/init.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Server } from '../Interfaces/server';
+import { Utilitaire } from '../../utilitaire';
 
 @Injectable()
 export class InitService {
   server_string : string ="[[Nom du serveur, Début, Nbr de tour, D des tours, Fin, Nombre de joueur, Etat], [astro, null, 0, 0, null, 3, 1]]";
-  constructor() { }
+  panel_server: JSON;
+  key_server: string[];
+  utilitaire: Utilitaire = new Utilitaire();
+  constructor() {
+    var JSONQury = {};
+    this.panel_server = this.utilitaire.StringToTable(this.server_string);
+    this.key_server = Object.keys(this.panel_server);
+    for (var key of this.key_server) {
+      this.panel_server[key] = this.createServer(key, this.panel_server);
+    }
+    console.log(this.panel_server);
+   }
   
+
+  createServer(name: string, json:JSON): Server{
+    let server: Server = {
+      id: name
+      , debut: ''
+      , nbrTourMax: Number(0)
+      , Dtours: Number(0)
+      , Fin: ''
+      , nbrJoueur: Number(0)
+      , Etat: ''
+    }
+    return server;
+  }
 }
