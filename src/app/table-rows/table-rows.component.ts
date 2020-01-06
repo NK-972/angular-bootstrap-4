915e75d4e420ff429456f5f03c6d193d87f9b132
NK-972 <nicolascastry@gmail.com>
2020-01-06 17:06:01 +0100

    2 files modified

--- src/app/table-rows/table-rows.component.ts
+++ src/app/table-rows/table-rows.component.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: any[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
